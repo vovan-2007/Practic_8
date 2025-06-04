1c144e8ac73c361b3d73f5775f45c155d3ed19e7
amogus <110690851+vovan-2007@users.noreply.github.com>
2025-06-04 12:35:50 +0400

    Add files via upload

---
+++ lab8t16/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace lab8t16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("data.txt");
+            string[] filtered = lines.Where(line => line.Length >= 5).ToArray();
+            File.WriteAllLines("filtered.txt", filtered);
+            Console.WriteLine($"Удалено {lines.Length - filtered.Length} строк");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab8t17/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace lab8t17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите математическое выражение: ");
+            string express = Console.ReadLine();
+            var result = new System.Data.DataTable().Compute(express, null);
+            Console.WriteLine($"Результат: {result}");
+            Console.WriteLine("Нажмите на любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
