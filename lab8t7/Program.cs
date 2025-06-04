332e5a078f26280a5b35d42aef2bf534788a42c2
amogus <110690851+vovan-2007@users.noreply.github.com>
2025-06-04 12:32:42 +0400

    Add files via upload

---
+++ lab8t7/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace lab8t7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string input = Console.ReadLine();
+            string[] words = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine($"Количество слов: {words.Length}");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab8t8/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace lab8t8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку с цифрами: ");
+            string input = Console.ReadLine();
+            string result = new string(input.Where(c => !char.IsDigit(c)).ToArray());
+            Console.WriteLine($"Результат: {result}");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
